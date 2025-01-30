51e0cd2e5ba7ded33350d6b43d69bed252a445f1
Nombulelo Khumalo <nombulelokhumalo48@gmail.com>
2025-01-30 15:44:06 +0200

    Fetch Input Links

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import Journal from "./components/UserInput/Journal";
+import AI from "./components/FetchInput/AI";
+import Analysis from "./components/FetchInput/Analysis";
+import Board from "./components/FetchInput/Board";
+import Home from "./components/FetchInput/Home";
 
 
 function App() {
@@ -44,6 +48,10 @@ function App() {
          <Routes>
           <Route path='/menu' element={<Menu/>}/>
           <Route path='/journal' element={<Journal/>}/>
+          <Route path="/ai" element={<AI/>}/>
+          <Route path="/analysis" element={<Analysis/>}/>
+          <Route path="/board" element={<Board/>}/>
+          <Route path="/" element={<Home/>}/>
          </Routes>
         </div>
         <Navbar/>
